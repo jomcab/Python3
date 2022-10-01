0c8c8c6e205798579eb454f47932b6e8bc9ca28d
jomercab <jomercabrera@gmail.com>
2022-10-01 17:27:06 +0800

    Function Arguments

---
+++ Function Arguments/unpacking_on_functioncalls.py
@@ -0,0 +1,7 @@
+def calculate_price_per_person(total, tip, split):
+  total_tip = total * (tip/100)
+  split_price = (total + total_tip) / split
+  print(split_price)
+
+table_7_total = [534.50, 20.0, 5]
+calculate_price_per_person(*table_7_total)
